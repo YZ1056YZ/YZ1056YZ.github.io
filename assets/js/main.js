1dab2f4a62bb3fd56ca79b43345132caf84619e2
YZ1056YZ <yz1056yz.posterior@outlook.com>
2025-03-27 23:06:31 -0500

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Main JavaScript file for Yunuo Zhang's academic website
+ * This provides interactive features and enhances user experience
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize all functions
+    initSmoothScrolling();
+    initMobileNavigation();
+    updateCopyrightYear();
+    addScrollAnimation();
+});
